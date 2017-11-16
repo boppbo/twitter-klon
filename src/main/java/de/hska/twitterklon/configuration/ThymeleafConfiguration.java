2dab707b4e80eb233550909c41e1ac15a443d36f
kaschka <schmidtdanielbirenbach@gmail.com>
2017-11-16 16:50:27 +0100

    Add ThymeleafConfiguration

---
+++ src/main/java/de/hska/twitterklon/configuration/ThymeleafConfiguration.java
@@ -0,0 +1,28 @@
+package de.hska.twitterklon.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+
+
+public class ThymeleafConfiguration {
+
+    @Bean
+    public SpringResourceTemplateResolver templateResolver(){
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setPrefix("/WEB-INF/templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        templateResolver.setCacheable(true);
+        return templateResolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine(){
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        templateEngine.setEnableSpringELCompiler(true);
+        return templateEngine;
+    }
+}
