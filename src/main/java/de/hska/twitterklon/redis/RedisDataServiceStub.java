3f37a6dc969450d3b11d98c832ce82ff1587d805
kaschka <schmidtdanielbirenbach@gmail.com>
2017-11-23 15:06:38 +0100

    Add Sessions to RedisDataServiceStub

--- src/main/java/de/hska/twitterklon/redis/RedisDataServiceStub.java
+++ src/main/java/de/hska/twitterklon/redis/RedisDataServiceStub.java
@@ -1,25 +1,45 @@
 package de.hska.twitterklon.redis;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.UUID;
+
+import javax.annotation.PostConstruct;
+
 import de.hska.twitterklon.api.transferobjects.PostDto;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Optional;
-
 @Service
 @Primary
 @Profile("DEV")
 public class RedisDataServiceStub implements RedisDataService {
+
+    private Map<String, String> sessions;
+
+    @PostConstruct
+    public void postConstruct() {
+        sessions = new HashMap<>();
+    }
+
     @Override
     public Optional<String> createSession(String userName, String password, int sessionDuration) {
-        return null;
+        String uuid = UUID.randomUUID().toString();
+        sessions.put(userName, uuid);
+        return Optional.of(uuid);
     }
 
     @Override
     public Optional<String> getUserNameFromSession(String sessionUUID) {
-        return null;
+        return sessions.entrySet()
+                .stream()
+                .filter(entry -> Objects.equals(entry.getValue(), sessionUUID))
+                .map(Map.Entry::getKey)
+                .findAny();
     }
 
     @Override
