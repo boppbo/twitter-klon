e91c2457e2d273253c4473b17f53f667a03faff8
Boris Bopp <boris.bopp@gmail.com>
2017-11-23 14:58:14 +0100

    Add username to post dto

--- src/main/java/de/hska/twitterklon/api/transferobjects/PostDto.java
+++ src/main/java/de/hska/twitterklon/api/transferobjects/PostDto.java
@@ -5,6 +5,7 @@ import java.security.Timestamp;
 public class PostDto {
     private String Content;
     private Timestamp CreationTime;
+    private String UserName;
 
     public String getContent() {
         return Content;
@@ -21,4 +22,8 @@ public class PostDto {
     public void setCreationTime(Timestamp creationTime) {
         CreationTime = creationTime;
     }
+
+    public String getUserName() { return UserName; }
+
+    public void setUserName(String userName) { UserName = userName; }
 }
