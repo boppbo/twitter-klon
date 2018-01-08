2a4ac7ca74ecd0036051c34198fb372710e750e1
Boris Bopp <boris.bopp@gmail.com>
2018-01-08 10:04:04 +0100

    Remove enable web mvc from config

--- src/main/java/de/hska/twitterklon/configuration/WebConfig.java
+++ src/main/java/de/hska/twitterklon/configuration/WebConfig.java
@@ -4,13 +4,11 @@ import de.hska.twitterklon.authentication.AuthenticationInterceptor;
 import de.hska.twitterklon.redis.RedisDataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Autowired
