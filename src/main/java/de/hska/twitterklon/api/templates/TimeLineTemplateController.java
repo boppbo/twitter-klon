413f9b62d65345a347c4e026603d15eaf7dc8a9f
Boris Bopp <boris.bopp@gmail.com>
2018-01-11 18:05:10 +0100

    TimeLine: Add getpost urls to template

--- src/main/java/de/hska/twitterklon/api/templates/TimeLineTemplateController.java
+++ src/main/java/de/hska/twitterklon/api/templates/TimeLineTemplateController.java
@@ -1,7 +1,5 @@
 package de.hska.twitterklon.api.templates;
 
-import de.hska.twitterklon.redis.RedisDataService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,32 +10,26 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/")
 public class TimeLineTemplateController {
-
-    private final RedisDataService redisDataService;
-
-    @Autowired
-    public TimeLineTemplateController(RedisDataService redisDataService) {
-        this.redisDataService = redisDataService;
-    }
-
     @RequestMapping(path = "global", method = RequestMethod.GET)
     public String globalTimeLine(Model model) {
+        model.addAttribute("postUrl", "/api/v1/timeline/global");
         return getTimeLineTemplate(model);
     }
 
     @RequestMapping(path = "user/{userName}", method = RequestMethod.GET)
     public String userTimeLine(@PathVariable String userName, Model model) {
+        model.addAttribute("postUrl", "/api/v1/timeline/user/" + userName);
         return getTimeLineTemplate(model);
     }
 
     @GetMapping
     public String selfTimeLine(Model model) {
+        model.addAttribute("postUrl", "/api/v1/timeline/self");
         return getTimeLineTemplate(model);
     }
 
     private String getTimeLineTemplate(Model model) {
         TemplateModelFiller.fillStandardParameters(model);
         return "posts";
-
     }
 }
