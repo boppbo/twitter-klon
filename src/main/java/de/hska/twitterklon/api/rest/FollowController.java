b9ac9ac8acf24d6ed57220f632eb413e898c040a
Boris Bopp <boris.bopp@gmail.com>
2018-01-12 00:35:33 +0100

    Fix resolving followers/following for different users

--- src/main/java/de/hska/twitterklon/api/rest/FollowController.java
+++ src/main/java/de/hska/twitterklon/api/rest/FollowController.java
@@ -1,28 +1,19 @@
 package de.hska.twitterklon.api.rest;
 
-import java.util.List;
-
-import javax.validation.Valid;
-
 import de.hska.twitterklon.api.transferobjects.UserTO;
 import de.hska.twitterklon.api.transferobjects.converters.UserToConverter;
 import de.hska.twitterklon.authentication.SessionInformation;
 import de.hska.twitterklon.redis.RedisDataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/follow")
 public class FollowController {
-
     private final RedisDataService redisDataService;
 
     @Autowired
@@ -44,10 +35,17 @@ public class FollowController {
         redisDataService.removeFollower(SessionInformation.getUserName(), userTO.getName());
     }
 
-    @GetMapping
+    @RequestMapping(path = "/user/{userName}/followers", method = RequestMethod.GET)
     @ResponseBody
     @ResponseStatus(HttpStatus.OK)
-    public List<UserTO> getFollowers() {
-        return UserToConverter.fromStringList(redisDataService.getFollower(SessionInformation.getUserName()));
+    public List<UserTO> getFollowers(@PathVariable("userName") String userName) {
+        return UserToConverter.fromStringList(redisDataService.getFollower(userName));
+    }
+
+    @RequestMapping(path = "/user/{userName}/following", method = RequestMethod.GET)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.OK)
+    public List<UserTO> getFollowing(@PathVariable("userName") String userName) {
+        return UserToConverter.fromStringList(redisDataService.getFollowing(userName));
     }
 }
