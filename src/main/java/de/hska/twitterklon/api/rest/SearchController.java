0ce07d727307b743d9db41d88911e89006aaaa51
Boris Bopp <boris.bopp@gmail.com>
2018-01-12 00:05:22 +0100

    Add search

--- src/main/java/de/hska/twitterklon/api/rest/SearchController.java
+++ src/main/java/de/hska/twitterklon/api/rest/SearchController.java
@@ -1,18 +1,13 @@
 package de.hska.twitterklon.api.rest;
 
-import java.util.List;
-
-import de.hska.twitterklon.api.transferobjects.UserTO;
-import de.hska.twitterklon.api.transferobjects.converters.UserToConverter;
+import de.hska.twitterklon.api.transferobjects.PostDto;
 import de.hska.twitterklon.redis.RedisDataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("api/v1/")
@@ -28,7 +23,7 @@ public class SearchController {
     @RequestMapping(path = "search", method = RequestMethod.GET)
     @ResponseBody
     @ResponseStatus(HttpStatus.OK)
-    public List<UserTO> searchForUser(@RequestParam String value, @RequestParam int count, @RequestParam int skipCount) {
-        return UserToConverter.fromStringList(redisDataService.searchUser(value, count, skipCount));
+    public List<PostDto> searchForUser(@RequestParam String value, @RequestParam(name = "skipCount", required = true) int skipCount) {
+        return redisDataService.searchUser(value, 16, skipCount).stream().map(u -> new PostDto("", "", u, "")).collect(Collectors.toList());
     }
 }
--- src/main/java/de/hska/twitterklon/api/templates/TimeLineTemplateController.java
+++ src/main/java/de/hska/twitterklon/api/templates/TimeLineTemplateController.java
@@ -12,19 +12,25 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class TimeLineTemplateController {
     @RequestMapping(path = "global", method = RequestMethod.GET)
     public String globalTimeLine(Model model) {
-        model.addAttribute("postUrl", "/api/v1/timeline/global");
+        model.addAttribute("postUrl", "/api/v1/timeline/global?foo=bar");
+        return getTimeLineTemplate(model);
+    }
+
+    @RequestMapping(path = "search", method = RequestMethod.GET)
+    public String search(String value, Model model) {
+        model.addAttribute("postUrl", "api/v1/search?value=" + value);
         return getTimeLineTemplate(model);
     }
 
     @RequestMapping(path = "user/{userName}", method = RequestMethod.GET)
     public String userTimeLine(@PathVariable String userName, Model model) {
-        model.addAttribute("postUrl", "/api/v1/timeline/user/" + userName);
+        model.addAttribute("postUrl", "/api/v1/timeline/user/" + userName + "?foo=bar");
         return getTimeLineTemplate(model);
     }
 
     @GetMapping
     public String selfTimeLine(Model model) {
-        model.addAttribute("postUrl", "/api/v1/timeline/self");
+        model.addAttribute("postUrl", "/api/v1/timeline/self?foo=bar");
         return getTimeLineTemplate(model);
     }
 
